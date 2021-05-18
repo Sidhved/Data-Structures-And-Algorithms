56e03eb0880ef04fb34514f3214fc4af05185633
Sidhved <66831453+Sidhved@users.noreply.github.com>
2021-05-18 12:15:12 +0530

    2 files uploaded

--- Python/DS/README.m
+++ Python/DS/README.m
@@ -2,4 +2,5 @@ The repository holds code for all the data structures in python.
 Follow the below list to jump to the topic of interest:
 
 DS:
-1. [Queue]
+1. [Queue](https://github.com/Sidhved/Data-Structures-And-Algorithms/blob/main/Python/DS/Queue.py)
+2. [Stack](https://github.com/Sidhved/Data-Structures-And-Algorithms/blob/main/Python/DS/Stack.py)
